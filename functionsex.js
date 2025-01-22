426f0710099b6921c49e3f2de9b1a6ff82a7dc3b
Trinishamuthavarapu <thrinishamuthavarapu@gmail.com>
2025-01-22 17:37:14 +0530

    custom msgs

---
+++ functionsex.js
@@ -0,0 +1,34 @@
+//! without parameters and arguments
+// function demo(){  //! function declaration
+//     console.log("Hello  i am function demo()");
+// }
+// demo();//! function calling
+// demo();
+// demo();
+// demo();
+// demo();
+// demo();
+// demo();
+// demo();
+// demo();
+// demo();
+
+
+
+//! with parameters and arguments
+// function add1(a,b){
+//     // console.log(a);
+//     // console.log(b);
+//     console.log(a+b);
+// }
+// add1(5,6);
+// add1(7,8);
+// add1(3,4);
+// add1(5,6);
+// add1(10,30);
+
+
+function addToCart(){
+    document.body.style.background="teal";
+    alert("Product is added to cart successfully")
+}
